3004ba02e1a87be5529950cfb5aa3653e28e661a
nil.sanca97 <nil.sanca97@gmail.com>
2026-04-27 10:39:24 +0200

    firebase installed and word

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,22 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({ 
+      apiKey: "AIzaSyDna2PwBf9M9VXd4bsFeAl9bjtA4KQZIgg",
+      authDomain: "projectm01-f4463.firebaseapp.com",
+      projectId: "projectm01-f4463",
+      storageBucket: "projectm01-f4463.firebasestorage.app",
+      messagingSenderId: "505409425956",
+      appId: "1:505409425956:web:f4b5d14b3931947163feb1",
+      measurementId: "G-LJ6JKSF918"
+    })),
+    provideAuth(() => getAuth()),
+  ],
 };
